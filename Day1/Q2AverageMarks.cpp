af767ae3d7089ecc7052de091ea57d1b6f4d2056
Sagar <141819298+CodeByAshuu@users.noreply.github.com>
2024-12-06 10:27:47 +0530

    Create Q2AverageMarks.cpp

---
+++ Day1/Q2AverageMarks.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  float n1,n2,n3,n4,n5;
+  cin >> n1 >> n2 >> n3 >> n4 >> n5;
+
+  float average = (n1 + n2 + n3 + n4 + n5) / 5;
+  cout << "Average : " << average << endl;
+}
